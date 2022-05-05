352fc3bb3f863aa630f03b76c1d89e13535fa193
Bruno Vieira <b.vieira@reply.com>
2022-05-05 20:55:09 -0300

    Replace _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,13 @@
-import Document,{Head, Html, Main, NextScript} from "Next/document"
+import Document,{Head, Html, Main, NextScript} from "next/document"
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
       <Head>
+      <title>share.news</title>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:ital,wght@0,400;1,600&family=Roboto:ital,wght@0,400;0,900;1,700&display=swap" rel="stylesheet"/>
-
-          <title>share.news</title>
       </Head>
       <body>
         <Main />
