17457376c48ab59c4a20cd11e7631e1862a13427
Bruno Vieira <b.vieira@reply.com>
2022-05-05 21:03:45 -0300

    Insert title by page using next/head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
       <Head>
-      <title>share.news</title>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:ital,wght@0,400;1,600&family=Roboto:ital,wght@0,400;0,900;1,700&display=swap" rel="stylesheet"/>
       </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
 import style from "../styles/Home.module.scss"
+import Head from "next/head"
 export default function Home() {
-  return <h1 className={style.title}>Hello <span>World</span></h1>
+  return (
+    <>
+      <Head>
+          <title>Inicio | share.news</title>
+      </Head>
+      <h1 className={style.title}>Hello 
+          <span> World</span>
+      </h1>
+    </>  
+  )
 }
