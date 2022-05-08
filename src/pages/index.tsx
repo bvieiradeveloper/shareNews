1af466ee5e92e2bb895c8fa664b566b6faceeec9
Bruno Vieira <b.vieira@reply.com>
2022-05-07 23:58:04 -0300

    Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
 import Head from "next/head"
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>
       <Head>
-          <title>Inicio | share.news</title>
+          <title>Home | share.news</title>
       </Head>
+      <main className={styles.containerContent}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the 
+              <span> React </span>
+            World
+          </h1>
+          <p>
+            Get acess to all the publication <br/>
+            <span>for $9.90 month</span>
+          </p>
+          <button type="button">Subscribe now</button>
+        </section>
+        <img src="/images/avatar.svg" alt="Avatar" />
+      </main>
     </>  
   )
 }
