fbaf7c2c2e4312057fdc8fadfcac142595a123e4
Bruno Vieira <b.vieira@reply.com>
2022-05-10 22:42:55 -0300

    Login with Nest.js(callbaks) and FaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,35 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/faunadb";
+import { query as q} from "faunadb";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
+      
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      try {
+
+      const { email } = user;
+        console.log(email);
+      await fauna.query(
+        q.Create(
+          q.Collection("users"),
+          { data: { email }}
+        )
+      )
+        return true;
+      } catch{
+        return true;
+      }
+    }
+  }, jwt: {
+    secret: process.env.JWT_SECRET,
+  },
 })
