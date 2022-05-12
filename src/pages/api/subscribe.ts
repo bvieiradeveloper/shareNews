99d87182d53049cde8fa4d5fad27a53bf6017c29
Bruno Vieira <b.vieira@reply.com>
2022-05-12 20:32:55 -0300

    Deny create recurring customer

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,20 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/faunadb";
+import { query as q} from "faunadb";
 import { stripe } from "../../services/stripe";
 
-
+type User = {
+  data:{
+    stripe_customer_id: string
+  }
+  ref:{
+    id: string
+  }
+}
 export default async (request: NextApiRequest, response: NextApiResponse) => {
 
   if(request.method === "POST"){
 
     const session = await getSession({ req:  request})
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user?.email
-    });
+    const user = await fauna.query<User>(q.Get(
+      q.Match(
+        q.Index('user_by_email'),
+        q.Casefold(session.user.email)
+      )
+    ))
+
+    let customerId = user.data.stripe_customer_id;
+
+    if(!customerId){
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user?.email
+      });
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data:{
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+      customerId: stripeCustomer.id
+    }
+  
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ['card'],
       allow_promotion_codes: true,
       billing_address_collection: 'required',
