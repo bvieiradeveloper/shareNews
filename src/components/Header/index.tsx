d19dcc60930ebf7354be7d356bb87d5abe248819
Bruno Vieira <b.vieira@reply.com>
2022-05-26 23:37:05 -0300

    Link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss"
+
 export function Header(){
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
+        <Link href="/">
           <a className={styles.active}>Home</a>
+        </Link>
+        <Link href="/posts" prefetch>
           <a>Posts</a>
+        </Link>
         </nav>
         <SignInButton/>
       </div>
