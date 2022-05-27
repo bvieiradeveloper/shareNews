eb658117234c5f8305bc5f09824a9a96762cf701
Bruno Vieira <b.vieira@reply.com>
2022-05-26 23:29:43 -0300

    Display Post Page Content

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,21 @@
 import { GetStaticProps } from "next";
 import  Head  from "next/head";
 import styles from './styles.module.scss';
-import { getPrismicClient } from "../../services/prismic"
-import Prismic from "@prismicio/client"
+import { getPrismicClient } from "../../services/prismic";
+import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
+interface Post{
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
 
-export default function Posts(){
+interface PostProps{
+  posts: Post[]
+}
+export default function Posts({posts}:PostProps){
 
   return(
     <>
@@ -14,21 +24,13 @@ export default function Posts(){
       </Head>
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a>
-            <time>12 de março de 2022</time>
-            <strong>Loren Text</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti ab porro eos aliquam quasi. Iure labore quo fuga quibusdam voluptatem a, magnam eius officia natus dicta earum amet, quos ab.</p>
+          { posts.map(post => (
+            <a key={post.slug}>
+            <time>{post.updatedAt}</time>
+            <strong>{post.title}</strong>
+            <p>{post.excerpt}</p>
           </a>
-          <a>
-            <time>12 de março de 2022</time>
-            <strong>Loren Text</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti ab porro eos aliquam quasi. Iure labore quo fuga quibusdam voluptatem a, magnam eius officia natus dicta earum amet, quos ab.</p>
-          </a>
-          <a>
-            <time>12 de março de 2022</time>
-            <strong>Loren Text</strong>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti ab porro eos aliquam quasi. Iure labore quo fuga quibusdam voluptatem a, magnam eius officia natus dicta earum amet, quos ab.</p>
-          </a>
+          ))}
         </div>
       </main>
     </>
@@ -38,15 +40,29 @@ export default function Posts(){
 export const getStaticProps: GetStaticProps = async () => {
 
   const prismic = getPrismicClient();
-  const response = await prismic.query([  
+  const response = await prismic.query<any>([  
     Prismic.predicates.at('document.type', 'post') //Aqui publication
   ], {
     fetch: ['title', 'content'],
     pageSize: 100
   });
 
+  const posts = response.results.map(post => {
+    return{
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: "long",
+        year: "numeric"
+      }),
+    }
+  })
   console.log(JSON.stringify(response, null, 2));
   return{
-    props:{}
+    props:{
+      posts
+    }
   }
 }
